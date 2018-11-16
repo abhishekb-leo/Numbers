d4d46027da3121d976babd14eee284ff02fe6120
Abhishek Bhattacharya <ab7607@rediffmail.com>
2018-11-16 08:58:27 +0530

    Created Fibonacci series code

---
+++ Fibonacci.ps1
@@ -0,0 +1,18 @@
+[cmdletBinding()]
+param(
+	[Parameter(Mandatory=$True,HelpMessage="Please enter a non-negative integer number upto which you want to find fibonacci series")]
+	[int]$number_from_user
+)
+
+clear
+write-host "A series of numbers in which each number (Fibonacci number) is the sum of the two preceding numbers. `nThe simplest is the series 1, 1, 2, 3, 5, 8, etc`n"
+
+write-host "This program will list the Fibonacci series numbers from 1 to $number_from_user" -foregroundcolor yellow
+write-host "======================================================" -foregroundcolor yellow
+$current=0
+$previous=1
+
+	while($current -lt $number_from_user){
+		$current; 
+		$current,$previous = ($current+$previous),$current
+	}
