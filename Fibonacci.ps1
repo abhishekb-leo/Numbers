ed0d5fc39b004273f30a23501d7197b7711fc720
Abhishek Bhattacharya <ab7607@rediffmail.com>
2018-11-16 09:04:41 +0530

    Formatting header with ==

--- Fibonacci.ps1
+++ Fibonacci.ps1
@@ -8,7 +8,7 @@ clear
 write-host "A series of numbers in which each number (Fibonacci number) is the sum of the two preceding numbers. `nThe simplest is the series 1, 1, 2, 3, 5, 8, etc`n"
 
 write-host "This program will list the Fibonacci series numbers from 1 to $number_from_user" -foregroundcolor yellow
-write-host "======================================================" -foregroundcolor yellow
+write-host "==============================================================" -foregroundcolor yellow
 $current=0
 $previous=1
 
